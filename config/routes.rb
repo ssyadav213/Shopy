9386038f18e716435c745a1c437e2586d135b441
Shiva <ssyadav213@gmail.com>
2017-09-24 10:40:19 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :categories
+  resources :products
+  get 'brand/index'
+
+  get 'home/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
